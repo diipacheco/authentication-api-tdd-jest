4f7df8c3fa8484db342655f4b343c01414855fad
diipacheco <edilsonpacheco6@gmail.com>
2019-09-05 16:54:27 -0300

    Configurate the test for the User model

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,10 +1,13 @@
+const { User } = require('../../src/app/models');
 
 describe('Authentication', () => {
-  it('should sum two numbers', () => {
-    const x = 2;
-    const y = 4;
-    const sum = x + y;
-
-    expect(sum).toBe(7);
+  it('should sum two numbers', async () => {
+    const user = await User.create({
+      name: 'Edilson Pacheco',
+      email: 'edilsonpacheco6@gmail.com',
+      password_hash: '123123',
+    });
+    console.log(user);
+    expect(user.email).toBe('edilsonpacheco6@gmail.com');
   });
 });
