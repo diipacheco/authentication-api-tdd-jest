488067908f222ec9b0e65a8b903892aacd383f13
diipacheco <edilsonpacheco6@gmail.com>
2019-09-05 17:33:48 -0300

    add the store function of the SessionController

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,13 +1,24 @@
+const request = require('supertest');
 const { User } = require('../../src/app/models');
+const app = require('../../src/app');
+const truncate = require('../utils/truncate');
 
 describe('Authentication', () => {
-  it('should sum two numbers', async () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+
+  it('should authenticate with valid credations', async () => {
     const user = await User.create({
       name: 'Edilson Pacheco',
       email: 'edilsonpacheco6@gmail.com',
       password_hash: '123123',
     });
-    console.log(user);
-    expect(user.email).toBe('edilsonpacheco6@gmail.com');
+    const response = await request(app).post('/sessions').send({
+      email: user.email,
+      password: '123456',
+    });
+    expect(response.status).toBe(200);
   });
 });
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 const routes = require('express').Router();
+const sessionControler = require('./app/controllers/sessionController');
 
+routes.post('/sessions', sessionControler.store);
 
 module.exports = routes;
