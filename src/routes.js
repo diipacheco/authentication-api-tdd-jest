eca3ad9621a6aba14a0732722e82c7cd0d95da18
diipacheco <edilsonpacheco6@gmail.com>
2019-09-05 16:07:31 -0300

    remove the User model from the routes file

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,4 @@
 const routes = require('express').Router();
-const { User } = require('./app/models');
 
-User.create({
-  name: 'Edilson',
-  email: 'edilson.pacheco@aoop.com.br',
-  password_hash: '18932781923719237as',
-});
 
 module.exports = routes;
