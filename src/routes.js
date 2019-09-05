3d7b30d82505f25fe16f462ded13d7f1e41e451e
diipacheco <edilsonpacheco6@gmail.com>
2019-09-05 15:58:30 -0300

    default route to create the User

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,10 @@
 const routes = require('express').Router();
+const { User } = require('./app/models');
+
+User.create({
+  name: 'Edilson',
+  email: 'edilson.pacheco@aoop.com.br',
+  password_hash: '18932781923719237as',
+});
 
 module.exports = routes;
