01f655077c6c807d622a2e6533df85861bd3c871
diipacheco <edilsonpacheco6@gmail.com>
2019-09-05 15:27:03 -0300

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '192.168.99.100',
+  username: 'docker',
+  password: 'docker',
+  database: 'nodeauth',
+  dialect: 'postgress',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
